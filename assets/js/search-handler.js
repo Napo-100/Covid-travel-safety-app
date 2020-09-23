973c2af5312dfbbe140c098d561205605cb91e61
John George <68780394+johngeorge88@users.noreply.github.com>
2020-09-22 21:25:05 -0400

    merging covid-api with develop

--- assets/js/search-handler.js
+++ assets/js/search-handler.js
@@ -1,6 +1,7 @@
 var searchBtn = document.getElementById("search-btn");
 var searchInput = document.getElementById("search-input");
 var searchHistory = document.getElementById("search-history");
+var display = document.querySelector(".main-container")
 
 
 // Search button function
@@ -9,7 +10,7 @@ var searchHandler = function (cityName) {
     var cityName = searchInput.value.trim();
     console.log(cityName);
     if (cityName) {
-        // getWeatherInfo(cityName);
+        getWeatherInfo(cityName);
         cityHistory(cityName);
         searchInput.value = "";
         getCountryOption();
@@ -18,6 +19,55 @@ var searchHandler = function (cityName) {
         swal("You entered an invalid city name!", "Please enter a valid one");
     }
 };
+
+// Weather function
+var getWeatherInfo = function (cityName) {
+
+    var weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=f28282748979d8ef4250a43282c46535";
+
+    fetch(weatherApi)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                    $("#Main-container").removeClass("hide")
+                    displayWeather(data)
+                    // cityHistory(cityName);
+                })
+            } else {
+                swal("You entered an invalid city name!", "Please enter a valid one");
+            }
+        })
+}
+
+// Display weather
+var displayWeather = function (data) {
+    var currentTemp = data.main.temp;
+    console.log(currentTemp)
+    var currentHumid = data.main.humidity;
+    console.log(currentHumid)
+    var currentWind = data.wind.speed;
+    console.log(currentWind)
+
+    var currentDate = moment().format("M/D/YYYY")
+    console.log(currentDate)
+
+    var iconDisplay = "<img src= 'http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png' />"
+    console.log(iconDisplay)
+
+    var weatherTitle = document.getElementById("weather-title")
+    var tempToday = document.querySelector("#temp-today")
+    var humidToday = document.querySelector("#humid-today")
+    var windToday = document.querySelector("#wind-today")
+    var iconToday = document.getElementById("icon-today")
+
+    // shows current weather
+    weatherTitle.innerHTML = "Current Weather"
+    iconToday.innerHTML = iconDisplay;
+    tempToday.innerHTML = "Temprature: " + currentTemp;
+    humidToday.innerHTML = "Humidity: " + currentHumid;
+    windToday.innerHTML = "Winds: " + currentWind;
+}
 
 // Adding city search to history 
 var cityHistory = function (city) {
@@ -75,5 +125,9 @@ var getStateCovidInfo = function (output) {
         })
 }
 
-
 searchBtn.addEventListener("click", searchHandler);
+searchInput.addEventListener("keyup", function (event) {
+    if (event.key === 13) {
+        searchHandler(cityName)
+    }
+});
