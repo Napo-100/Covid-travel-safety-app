2f46303667ee752467ffe10c844e0df6e4334e9a
Jrsparl <john@sparlingmusic.com>
2020-09-24 21:48:39 -0400

    Autocomplete pulling information basic

---
+++ assets/js/google-places-testing.js
@@ -0,0 +1,21 @@
+// maps.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+// var autocomplete = new google.maps.places.Autocomplete(input);
+
+//https://maps.googleapis.com/maps/api/place/findplacefromtext/output?parameters
+
+
+// function initAutocomplete() {
+//     // Create the autocomplete object, restricting the search predictions to
+//     // geographical location types.
+//     autocomplete = new google.maps.places.Autocomplete(
+//       document.getElementById("search-input"),
+//       { types: ["geocode"] }
+//     );
+//     // Avoid paying for data that you don't need by restricting the set of
+//     // place fields that are returned to just the address components.
+//     autocomplete.setFields(["address_component"]);
+//     // When the user selects an address from the drop-down, populate the
+//     // address fields in the form.
+//     autocomplete.addListener("place_changed", fillInAddress);
+//   };
+
